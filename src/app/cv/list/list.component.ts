0b8f54c87d790d88587e4fbb556b3de53d2680e2
aymen2782 <aymen.sellaouti@gmail.com>
2021-04-28 09:23:27 +0100

    cvTech sprint 2

--- src/app/cv/list/list.component.ts
+++ src/app/cv/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Cv } from '../model/cv';
 
 @Component({
@@ -8,13 +8,41 @@ import { Cv } from '../model/cv';
 })
 export class ListComponent implements OnInit {
   cvs: Cv[] = [];
+  @Output() forwardCv = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {
     this.cvs = [
-      new Cv(1, 'sellaouti', 'aymen', 'teacher', 'rotating_card_profile2.png', 38, 1234),
-      new Cv(2, 'chaouachi', 'amira', 'Dev', 'rotating_card_profile.png', 22, 1258),
-      new Cv(3, 'mejri', 'marwen', 'Dev', 'rotating_card_profile3.png', 24, 5874),
+      new Cv(
+        1,
+        'sellaouti',
+        'aymen',
+        'teacher',
+        'rotating_card_profile2.png',
+        38,
+        1234
+      ),
+      new Cv(
+        2,
+        'chaouachi',
+        'amira',
+        'Dev',
+        'rotating_card_profile.png',
+        22,
+        1258
+      ),
+      new Cv(
+        3,
+        'mejri',
+        'marwen',
+        'Dev',
+        'rotating_card_profile3.png',
+        24,
+        5874
+      ),
     ];
+  }
+  forwardItem(cv: Cv): void {
+    this.forwardCv.emit(cv);
   }
 }
